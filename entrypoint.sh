b76baec71528f8467805abce1b0ebb32542c2ab4
lfgberg <lfgaberg@gmail.com>
2025-04-03 17:35:09 -0400

    undo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@
 grafana-cli plugins install marcusolsson-json-datasource
 
 cp /custom-grafana-db/grafana.db /var/lib/grafana/grafana.db
-chmod 777 /etc/grafana/grafana.ini
-chmod 777 /var/lib/grafana/grafana.db
 
 # Proceed with the default Grafana entrypoint
 exec /bin/bash /run.sh
