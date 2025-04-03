538d171ec9f526f17f6ef2fa0dbd3e55791e0940
lfgberg <lfgaberg@gmail.com>
2025-04-03 17:45:13 -0400

    fix group

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@
 grafana-cli plugins install marcusolsson-json-datasource
 
 cp /custom-grafana-db/grafana.db /var/lib/grafana/grafana.db
+chown grafana:grafana /var/lib/grafana/grafana.db
+chmod 660 /var/lib/grafana/grafana.db
 
 # Proceed with the default Grafana entrypoint
 exec /bin/bash /run.sh
